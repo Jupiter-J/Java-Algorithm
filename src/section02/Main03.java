34e3c1a5b9a3e60b26b4b05fea63e9d7c21967f2
Jupiter-J <namju2912@gmail.com>
2022-07-30 11:25:34 +0900

    feat: 0730_section01

---
+++ src/section02/Main03.java
@@ -0,0 +1,24 @@
+package section02;
+
+import java.util.Scanner;
+
+public class Main03 {
+    public static String solution(String str){
+        String answer = "";
+        int m = Integer.MIN_VALUE; //최댓값을 구하기 위해 가장 작은값으로 초기화
+        String[] s= str.split(" "); //""기준으로 분리하여 배열에 넣겠다
+        for (String x: s){
+            int len= x.length(); //각각의 단어의 길이를 len에 저장
+            if (len>m){
+                m=len; //최댓값은 m으로 갱신
+                answer=x;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+}
